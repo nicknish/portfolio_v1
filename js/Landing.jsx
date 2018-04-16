2c0af4497dbf6f2af998661390f4024b04beb768
nicknish <nickjnish@gmail.com>
2018-04-16 22:20:34 +0900

    Fix Landing copy and eslint errors

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom';
 
 const Landing = () => (
   <div>
-    <section className="container">
+    <section className="container hero">
       <h1>
-        Hi I'm Nick! I am a Front End / Full Stack Developer living in Los Angeles, CA.
+        Hi I
+        {"'"}
+        m Nick! I am a Front End / Full Stack Developer living in Los Angeles, CA.
       </h1>
     </section>
 
@@ -15,12 +17,26 @@ const Landing = () => (
 
     <section className="container">
       <h3>About</h3>
-      <p>You, scary? Ha ha ha</p>
+      <p>
+        I
+        {"'"}
+        m Nick, a passionate developer with a deep love for creating great products and the craft of telling important stories. Because my love for frontend, I consider myself a frontend developer first, but I’m wonderfully full-stack — JavaScript, React/Redux, Ruby, Rails, are all fair game.
+      </p>
+      <p>
+        I get stoked over conversations of product, design, and brand building.
+      </p>
+      <p>
+        I meditate, learn everyday without fail, and strive to carry joy in everything I do.
+      </p>
     </section>
 
     <section className="container">
       <h3>Experience</h3>
-      <p>Stuff about Bonsai, etc. etc.</p>
+      <p>
+        I
+        {"'"}
+        ve worked for Andreessen-Horowitz and YCombinator companies alike. The only through-line is that I love to work with other passionate folk who care about being kind.
+      </p>
       <Link to="/work">See Work</Link>
       <Link to="/projects">See Projects</Link>
     </section>
@@ -28,7 +44,7 @@ const Landing = () => (
     <section className="container">
       <h3>More</h3>
       <p>
-        Other than work, I do some <a href="https://nicknish.blog">writing</a>.
+        I do some <a href="https://nicknish.blog">writing</a>.
       </p>
     </section>
   </div>
