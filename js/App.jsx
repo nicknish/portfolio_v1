4d9a6262082833eaee492a581af7b49bd8bcf07b
nicknish <nickjnish@gmail.com>
2018-04-29 15:24:13 +0900

    Add ScrollToTop HOC to wrap app so React Router page changes will scroll to top

--- js/App.jsx
+++ js/App.jsx
@@ -8,6 +8,8 @@ import faCaretLeft from '@fortawesome/fontawesome-free-solid';
 import faGithubAlt from '@fortawesome/fontawesome-free-brands/faGithubAlt';
 import faTwitter from '@fortawesome/fontawesome-free-brands/faTwitter';
 
+import ScrollToTop from './ScrollToTop';
+
 // Pages
 import NotFound from './NotFound';
 import Header from './Header';
@@ -25,18 +27,20 @@ fontawesome.library.add(faCaretLeft, faGithubAlt, faTwitter);
 const App = () => (
   <div>
     <BrowserRouter>
-      <div className="site-container">
-        <Header />
-        <main className="site-content">
-          <Switch>
-            <Route path="/" exact component={Landing} />
-            <Route path="/work" component={WorkExperience} />
-            <Route path="/projects" component={Projects} />
-            <Route component={NotFound} />
-          </Switch>
-        </main>
-        <Footer />
-      </div>
+      <ScrollToTop>
+        <div className="site-container">
+          <Header />
+          <main className="site-content">
+            <Switch>
+              <Route path="/" exact component={Landing} />
+              <Route path="/work" component={WorkExperience} />
+              <Route path="/projects" component={Projects} />
+              <Route component={NotFound} />
+            </Switch>
+          </main>
+          <Footer />
+        </div>
+      </ScrollToTop>
     </BrowserRouter>
   </div>
 );
