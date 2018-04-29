60384216617e9e6d52cf69a3f10be0d796371d42
nicknish <nickjnish@gmail.com>
2018-04-29 14:42:22 +0900

    Update NotFound styling and content

--- js/NotFound.jsx
+++ js/NotFound.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const NotFound = () => <h1>Not Found</h1>;
+const NotFound = () => (
+  <div className="container">
+    <h1>Sorry, there{"'"}s nothing here!</h1>
+    <p>
+      That
+      {"'"}
+      s weird. You found a dead-end. Click below to go back to the good stuff.
+    </p>
+    <Link to="/" className="btn btn-primary btn-sm">
+      Go Home
+    </Link>
+  </div>
+);
 
 export default NotFound;
